a11a06cead050fecb3599019344a58f03c0ca106
Ismael <s.g.ismaelcadenas@gmai.com>
2022-11-04 10:27:11 +0100

    Añado comentarios

--- JS.js
+++ JS.js
@@ -1,7 +1,4 @@
+//Cambio de color al body
 function cambiarColor(){
     document.body.style.backgroundColor='#DDE0E3';
 }
-
-// Aplica al botón principal la funcionalidad de:
-
-// al clicar sobre el mismo cambiará el color del fondo de pantalla por un color fijo, de color gris claro (DDE0E3)
